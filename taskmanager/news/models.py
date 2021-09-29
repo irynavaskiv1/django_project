430f839a8914db8e8c2d151f133570c08e5cbc23
iryna.vaskiv <Iryna.Vaskiv@enghouse.com>
2021-09-29 17:37:42 +0300

    add model to db, make migration

--- taskmanager/news/models.py
+++ taskmanager/news/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Articles(models.Model):
+    title = models.CharField('Назва', max_length=30, default='Цікаве про село')
+    anons = models.CharField('Анонс', max_length=100, default='Анонс про подію')
+    text = models.TextField('Стаття')
+    date = models.DateTimeField('Дата публікації')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Новина'
+        verbose_name_plural = 'Новини'
