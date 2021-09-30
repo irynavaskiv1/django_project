eb0d9bdeca7d6ae68fdd1c1d7bff6edacb00e508
iryna.vaskiv <Iryna.Vaskiv@enghouse.com>
2021-09-30 16:38:04 +0300

    add titles to db

--- taskmanager/news/views.py
+++ taskmanager/news/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Articles
 
 
 def news(request):
-    return render(request, 'main/news.html')
+    news_obj = Articles.objects.order_by('-date')
+    return render(request, 'main/news.html', {'news_obj': news_obj})
