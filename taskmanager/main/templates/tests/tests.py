aeed92edefef5e76f918e27f4e8f88500cce73a5
iryna.vaskiv <Iryna.Vaskiv@enghouse.com>
2021-10-04 00:01:52 +0300

    update architecture

--- taskmanager/main/templates/tests/tests.py
+++ taskmanager/main/templates/tests/tests.py
@@ -1,3 +1,14 @@
+from django.urls import resolve
 from django.test import TestCase
+from django.http import HttpRequest
 
-# Create your tests here.
+from taskmanager.main.templates.main import about
+
+
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_home_page(self):
+        request = HttpRequest()
+        responce = about(request)
+        html = responce.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
