a9cd3213219b2dfe328ff25e898ea5e7c92ff77b
iryna.vaskiv <Iryna.Vaskiv@enghouse.com>
2021-10-04 18:56:01 +0300

    update image functional testing

--- taskmanager/main/templates/tests/functional/test_image.py
+++ taskmanager/main/templates/tests/functional/test_image.py
@@ -1,58 +1,29 @@
-import const
 import unittest
 
+from . import const
 from . import base_class
+from bs4 import BeautifulSoup
 
 
 class AllTitlesTestSuite(base_class.BaseSetUp):
+    soup = BeautifulSoup(const.URL)
+    URL = soup.currentTag.contents[0]
 
-    def test_about_us_page(self):
-        self.browser.get(const.URL)
-        image = self.browser.find_element_by_xpath('/html/body/div[3]/main/div/div[2]/div/div[1]/div/div/div/div')
-        is_image_exist = True if 'align-left' in image.get_attribute('class') else False
-        self.assertTrue(is_image_exist)
+    def test_main_image(self):
+        pages = ['', '/history', '/kindergarten', '/school', '/lyceum', '/religion', '/restaurant', '/contacts']
+        for page in pages:
+            self.browser.get(AllTitlesTestSuite.URL + page)
+            image = self.browser.find_element_by_xpath('/html/body/div[3]/main/div/div[2]/div/div[1]/div/div/div/div')
+            is_image_exist = True if 'align-left' in image.get_attribute('class') else False
+            self.assertTrue(is_image_exist, msg='Main image exists')
 
-    def test_history_page(self):
-        self.browser.get(const.URL + '/history')
-        image = self.browser.find_element_by_xpath('/html/body/div[3]/main/div/div[2]/div/div[1]/div/div/div/div')
-        is_image_exist = True if 'align-left' in image.get_attribute('class') else False
-        self.assertTrue(is_image_exist)
-
-    def test_kindergarten_page(self):
-        self.browser.get(const.URL + '/kindergarten')
-        image = self.browser.find_element_by_xpath('/html/body/div[3]/main/div/div[2]/div/div[1]/div/div/div/div')
-        is_image_exist = True if 'align-left' in image.get_attribute('class') else False
-        self.assertTrue(is_image_exist)
-
-    def test_school_page(self):
-        self.browser.get(const.URL + '/school')
-        image = self.browser.find_element_by_xpath('/html/body/div[3]/main/div/div[2]/div/div[1]/div/div/div/div')
-        is_image_exist = True if 'align-left' in image.get_attribute('class') else False
-        self.assertTrue(is_image_exist)
-
-    def test_lyceum_page(self):
-        self.browser.get(const.URL + '/lyceum')
-        image = self.browser.find_element_by_xpath('/html/body/div[3]/main/div/div[2]/div/div[1]/div/div/div/div')
-        is_image_exist = True if 'align-left' in image.get_attribute('class') else False
-        self.assertTrue(is_image_exist)
-
-    def test_religion_page(self):
-        self.browser.get(const.URL + '/religion')
-        image = self.browser.find_element_by_xpath('/html/body/div[3]/main/div/div[2]/div/div[1]/div/div/div/div')
-        is_image_exist = True if 'align-left' in image.get_attribute('class') else False
-        self.assertTrue(is_image_exist)
-
-    def test_restaurant_page(self):
-        self.browser.get(const.URL + '/restaurant')
-        image = self.browser.find_element_by_xpath('/html/body/div[3]/main/div/div[2]/div/div[1]/div/div/div/div')
-        is_image_exist = True if 'align-left' in image.get_attribute('class') else False
-        self.assertTrue(is_image_exist)
-
-    def test_contacts_page(self):
-        self.browser.get(const.URL + '/contacts')
-        image = self.browser.find_element_by_xpath('/html/body/div[3]/main/div/div[2]/div/div[1]/div/div/div/div')
-        is_image_exist = True if 'align-left' in image.get_attribute('class') else False
-        self.assertTrue(is_image_exist)
+    def test_logo_image(self):
+        pages = ['', '/history', '/kindergarten', '/school', '/lyceum', '/religion', '/restaurant', '/contacts']
+        for page in pages:
+            self.browser.get(AllTitlesTestSuite.URL + page)
+            logo_image = self.browser.find_element_by_xpath('//*[@id="layoutcontainer"]/div[6]/header/div[4]/div/a/img')
+            is_logo_exist = True if 'img' in logo_image.tag_name else False
+            self.assertTrue(is_logo_exist, msg='Logo image exists')
 
 
 def suite():
