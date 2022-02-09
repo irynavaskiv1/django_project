eed83badfc2b565adc0d8f4b15f7bdda649c44bd
iryna.vaskiv <Iryna.Vaskiv@enghouse.com>
2022-02-09 13:16:25 +0200

    add e2e test test_html_all_pages

--- taskmanager/main/tests-django/tests/e2e/test_html_all_pages.py
+++ taskmanager/main/tests-django/tests/e2e/test_html_all_pages.py
@@ -1,14 +1,16 @@
 from pytest_django.asserts import assertTemplateUsed
 from selenium import webdriver
 
+from consts import (EXECUTABLE_PATH, HTML_EXTENSION, POHIRTSI_SPACE_TABS_LINKS,
+                    URL)
+
 
 def test_html_all_pages():
     """ test that home page returns correct html"""
-    executable_path = '/Users/admin/Documents/django_project/taskmanager/main/tests-django/drivers/chromedriver'
-    url = 'http://www.pohirtsi.space'
-    pages = ['/history', '/kindergarten', '/school', '/lyceum', '/religion', '/restaurant', '/contacts']
+    pages = [POHIRTSI_SPACE_TABS_LINKS.about, POHIRTSI_SPACE_TABS_LINKS.history, POHIRTSI_SPACE_TABS_LINKS.kindergarten,
+             POHIRTSI_SPACE_TABS_LINKS.school, POHIRTSI_SPACE_TABS_LINKS.lyceum, POHIRTSI_SPACE_TABS_LINKS.religion,
+             POHIRTSI_SPACE_TABS_LINKS.restaurant, POHIRTSI_SPACE_TABS_LINKS.news, POHIRTSI_SPACE_TABS_LINKS.contacts]
     for page in pages:
-        browser = webdriver.Chrome(executable_path=executable_path)
-        responce = browser.get(url + page)
-        result_html = page.split('/')[1] + '.html'
-        assertTemplateUsed(responce, result_html)
+        browser = webdriver.Chrome(executable_path=EXECUTABLE_PATH)
+        result_html = page.split('/')[1] + HTML_EXTENSION
+        assertTemplateUsed(browser.get(URL + page), result_html)
