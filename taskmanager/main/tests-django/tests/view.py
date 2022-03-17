267cc1358ef3eb88d3b284996d1616b2c815131c
iryna.vaskiv <Iryna.Vaskiv@enghouse.com>
2022-03-17 21:39:51 +0200

    add view

---
+++ taskmanager/main/tests-django/tests/view.py
@@ -0,0 +1,15 @@
+from collections import namedtuple
+
+# menu bar buttons
+logo = ''
+PohirtsiSpaceButtonsIds = namedtuple(
+    "PohirtsiSpaceTabsLinks", "about history kindergarten school lyceum religion restaurant news contacts")
+POHIRTSI_SPACE_BUTTONS_IDS = PohirtsiSpaceButtonsIds(about='menu_item-1', history='menu_item-2',
+                                                     kindergarten='menu_item-3', school='menu_item-4',
+                                                     lyceum='menu_item-5', religion='menu_item-6',
+                                                     restaurant='menu_item-7', news='menu_item-8',
+                                                     contacts='menu_item-9')
+BUTTONS = [POHIRTSI_SPACE_BUTTONS_IDS.about, POHIRTSI_SPACE_BUTTONS_IDS.history,
+           POHIRTSI_SPACE_BUTTONS_IDS.kindergarten, POHIRTSI_SPACE_BUTTONS_IDS.school,
+           POHIRTSI_SPACE_BUTTONS_IDS.lyceum, POHIRTSI_SPACE_BUTTONS_IDS.religion,
+           POHIRTSI_SPACE_BUTTONS_IDS.restaurant, POHIRTSI_SPACE_BUTTONS_IDS.news, POHIRTSI_SPACE_BUTTONS_IDS.contacts]
