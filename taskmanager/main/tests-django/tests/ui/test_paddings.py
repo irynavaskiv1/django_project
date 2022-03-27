2905d2d91c21f27b06868011e1206d657651ee62
iryna.vaskiv <Iryna.Vaskiv@enghouse.com>
2022-03-27 23:20:34 +0300

    add test_paddings tc description

--- taskmanager/main/tests-django/tests/ui/test_paddings.py
+++ taskmanager/main/tests-django/tests/ui/test_paddings.py
@@ -3,5 +3,16 @@ import pytest
 
 @pytest.mark.skip
 def test_paddings():
-    pass
+    """
+    @ID: 008
+    @Category: ui
+    @Description: test that all pages returns correct padding
+    @tcmethod: automated
 
+    Steps:
+        1. Click to each page in site
+
+    Expected:
+        1. In each page paddings (Погірці, 2021 - 2025) is visible and correct
+    """
+    pass
