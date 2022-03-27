8ce86b04b6b4379f78351964cecf951edf6c6282
iryna.vaskiv <Iryna.Vaskiv@enghouse.com>
2022-03-27 23:19:21 +0300

    add test_main_words tc description

--- taskmanager/main/tests-django/tests/ui/test_main_words.py
+++ taskmanager/main/tests-django/tests/ui/test_main_words.py
@@ -3,4 +3,16 @@ import pytest
 
 @pytest.mark.skip
 def test_main_words():
+    """
+    @ID: 007
+    @Category: ui
+    @Description: test that all pages returns correct main words
+    @tcmethod: automated
+
+    Steps:
+        1. Click to each page in site
+
+    Expected:
+        1. In each page main words (Про нас/Історія/Садок..) is visible and correct
+    """
     pass
